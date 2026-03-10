750485e35a8646315ef5c7db8ac511573407f241
POTHANA PARDHU <pardhupothana2004@gmail.com>
2026-03-10 18:40:41 +0530

    Update api-config.js

--- js/api-config.js
+++ js/api-config.js
@@ -1,9 +1,9 @@
 // js/api-config.js
 const CONFIG = {
-    AMADEUS_ID: 't0u718Gnd95HmGAM47hdMrpSeJejPC8l',
-    AMADEUS_SECRET: '3ljjDBEyL819r10l',
-    UNSPLASH_KEY: 'WbtbKdmLnz6M8h7Y97D62oSNTSnEb3A0WCi5rezls6w',
+    AMADEUS_ID: '',
+    AMADEUS_SECRET: '',
+    UNSPLASH_KEY: '',
     // Amadeus requires a proxy for frontend requests to avoid CORS errors
     // PROXY_URL: 'https://corsproxy.io/?'
-    OPENWEATHER_KEY: 'c2975e53e18fe593ed16a4a6090f825e'
+    OPENWEATHER_KEY: ''
 };
